dbefe83b15c88dceccb7e2e9cf78688f74582839
babyjezus17 <61850116+babyjezus17@users.noreply.github.com>
2020-08-28 16:50:57 -0400

    Create stock_pred.py

---
+++ stock_pred.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import *
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
