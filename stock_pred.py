92a627d75da851027d07e152d31333cb2c010dd6
babyjezus17 <61850116+babyjezus17@users.noreply.github.com>
2020-08-29 18:47:40 -0400

    Modified stock_pred

--- stock_pred.py
+++ stock_pred.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,3 +8,43 @@ from tensorflow.keras import *
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+
+path = "C:/Users/Jesus/Documents/GitHub/stockpredictionsdataset/stocks/"
+filenames = []
+for fname in os.listdir(path):
+    filenames.append(fname)
+
+
+df = pd.read_csv('{}{}'.format(path, filenames[0]))
+
+for i in range(df.shape[0] - 1):
+    if int(df['Date'][i][:4]) < 2015:
+        df = df.drop([i])
+    else:
+        break
+df = df.reset_index(drop=True)
+
+
+
+rolling = pd.DataFrame({'Date': [], 'Open': [], 'High': [], 'Low': [],
+                        'Close': [], 'Adj close' : [], 'Volume': []})
+
+
+month = int(df.Date.iloc[0][5:7])
+sums = [0, 0, 0, 0, 0, 0]
+days = 0
+for i in range(df.shape[0]):
+    #breakpoint()
+    if int(df.Date.iloc[i][5:7]) == month:
+        for j in range(6):
+            sums[j] += df.iloc[i][j+1]
+        days += 1
+    else:
+        for j in range(6):
+            sums[j] = sums[j]/days
+        final = [df.Date.iloc[i-1][:7]] + sums
+        rolling.append(final)
+        month = df.Date.iloc[i][5:7]
+
+print(rolling)
